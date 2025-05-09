b8e7bdd1a2ec20674969edfcc8974e02966a0f76
gidori <gidori.nam@samsung.com>
2025-05-09 21:05:51 +0900

    Define gidori_ioctrl

--- os/drivers/gidori/gidori.c
+++ os/drivers/gidori/gidori.c
@@ -83,7 +83,28 @@ static int gidori_write(FAR struct file *filep)
 
 static int gidori_ioctrl(FAR struct file *filep, int cmd, unsigned long arg) 
 {
-    return OK;
+    FAR struct inode *inode = filep->f_inode;
+	FAR struct gidori_dev_buf *dev = inode->i_private;
+
+    int ret = -ENOSYS;
+
+    switch(cmd){
+    case GIDORI_WRITE:
+        if (strlen((char*)arg) > BUFFER_SIZE){
+            ret = ENOBUFS;
+            break;
+        }
+        memcpy(dev->buffer, (char*)arg, strlen((char*)arg)+1);
+        ret = OK;
+        break;
+    case GIDORI_READ:
+        memcpy((char*)arg, dev->buffer, strlen(dev->buffer)+1);
+        ret = OK;
+        break;
+    default:
+        ret = EINVAL;
+        break;
+    }
 }
 
 /****************************************************************************
