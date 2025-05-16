b237008fd92b2d5aa0dae27d389541b7a3df250d
gidori <gidori.nam@samsung.com>
2025-05-16 16:02:52 +0900

    Add test code of block device and mount point device.

--- apps/system/utils/utils_gidori.c
+++ apps/system/utils/utils_gidori.c
@@ -6,19 +6,76 @@
 #include <sys/ioctl.h>
 #include <tinyara/prodconfig.h>
 #include <tinyara/fs/ioctl.h>
+#include <tinyara/fs/fs_utils.h>
+
+
 
 int utils_gidori(int argc, char **args)
 {
-    char buf[BUFFER_SIZE];
+    if ((argc != 2) || !strncmp(args[1], "--help", strlen("--help") + 1)) 
+    {
+        printf("\nUsage : gidori fs_type\n");
+		printf("charactor device\t= 1\n");
+        printf("block device\t\t= 2\n");
+        printf("mnt block device\t= 3\n");
+        return -1;
+    }
 
-    int fd = open("/dev/gidori", O_RDWR);
-    
-    ioctl(fd, GIDORI_WRITE, (unsigned long)"babo");
-    ioctl(fd, GIDORI_READ, (unsigned long)buf);
+    if (!strcmp(args[1], "1"))
+    {
+        printf("[TEST] charactor device\n");
 
-    printf("buf : %s \n", (char *)buf);
+        int fd;
+        char buf[BUFFER_SIZE];
 
-	close(fd);
+        fd = open("/dev/gidori", O_RDWR);
+        
+        ioctl(fd, GIDORI_WRITE, (unsigned long)"babo");
+        ioctl(fd, GIDORI_READ, (unsigned long)buf);
+        close(fd);
+
+        printf("buf : %s \n", (char *)buf);
+    }
+    else if (!strcmp(args[1], "2"))
+    {
+        printf("[TEST] mtd block device\n");
+        
+        int fd;
+        char buf[BUFFER_SIZE];
+
+        fd = open("/dev/mtdblock9", O_RDWR);
+
+    	lseek(fd, 0, SEEK_SET);
+    	write(fd, "babo", 5);
+    	lseek(fd, 0, SEEK_SET);
+    	read(fd, buf, 5);
+        close(fd);
+
+        printf("buf : %s \n", buf);
+    }
+    else if (!strcmp(args[1], "3"))
+    {
+        printf("[TEST] mount point device\n");
+		
+        int fd;
+        char buf[BUFFER_SIZE];
+
+		fd = open("/mnt/smartfs/test.txt", O_WRONLY | O_CREAT, 0644);
+        write(fd, "babo", 5);
+        close(fd);
+        
+        fd = open("/mnt/smartfs/test.txt", O_RDONLY, 0644);
+		read(fd, buf, 5);
+        close(fd);
+        		
+		printf("buf : %s \n", buf);
+    }
+    else
+    {
+        printf("Invalid input");
+        return 1;
+    }
+
 
     return OK;
 }
